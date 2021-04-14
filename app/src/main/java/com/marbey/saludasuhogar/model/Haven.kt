ee1f4a12adb420814167aebc7ca4c909edab39df
Sebastian Ariza <sebas.camilo012@hotmail.com>
2021-04-14 02:03:32 -0500

    Finalización inicial de las interfaces

---
+++ app/src/main/java/com/marbey/saludasuhogar/model/Haven.kt
@@ -0,0 +1,5 @@
+package com.marbey.saludasuhogar.model
+
+class Haven {
+    
+}
--- app/src/main/java/com/marbey/saludasuhogar/ui/fragments/HavenFragment.kt
+++ app/src/main/java/com/marbey/saludasuhogar/ui/fragments/HavenFragment.kt
@@ -14,10 +14,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [havenFragment.newInstance] factory method to
+ * Use the [HavenFragment.newInstance] factory method to
  * create an instance of this fragment.
  */
-class havenFragment : Fragment() {
+class HavenFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -45,12 +45,12 @@ class havenFragment : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment havenFragment.
+         * @return A new instance of fragment HavenFragment.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            havenFragment().apply {
+            HavenFragment().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
